40e024cf726fe8588218c66871e36cf57230f18c
Adrián Garcia Lomas <glomadrian@gmail.com>
2017-03-25 15:33:56 +0100

    Add attributes for grav ball animation

--- grav/src/main/java/com/github/glomadrian/grav/generator/animation/GravBallSizeAnimation.java
+++ grav/src/main/java/com/github/glomadrian/grav/generator/animation/GravBallSizeAnimation.java
@@ -11,10 +11,12 @@ import com.github.glomadrian.grav.figures.GravBall;
 public class GravBallSizeAnimation extends GravAnimatorGenerator<GravBall> {
   private long minAnimationDuration = 2000;
   private long maxAnimationDuration = 3000;
+  private int fromSize = 0;
+  private int toSize = 40;
 
   @Override
   protected ValueAnimator createValueAnimator(GravBall grav, int width, int height) {
-    ValueAnimator valueAnimator = ValueAnimator.ofFloat(0 , 40);
+    ValueAnimator valueAnimator = ValueAnimator.ofFloat(fromSize , toSize);
     valueAnimator.setDuration(getRandomDuration(minAnimationDuration, maxAnimationDuration));
     valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
     valueAnimator.setRepeatMode(ValueAnimator.REVERSE);
@@ -40,5 +42,7 @@ public class GravBallSizeAnimation extends GravAnimatorGenerator<GravBall> {
   public void configure(TypedArray attributeSet, Context context) {
     minAnimationDuration = attributeSet.getInteger(R.styleable.GravView_min_animation_time, (int) minAnimationDuration);
     maxAnimationDuration = attributeSet.getInteger(R.styleable.GravView_max_animation_time, (int) maxAnimationDuration);
+    fromSize = attributeSet.getInteger(R.styleable.GravView_grav_ball_size_anim_from_size, fromSize);
+    toSize = attributeSet.getInteger(R.styleable.GravView_grav_ball_size_anim_to_size, toSize);
   }
 }
